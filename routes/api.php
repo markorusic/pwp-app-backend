ed6ed4ab5f4b20d1ee3f450be90fd68e18eb62af
Marko Rusic <markousp5@gmail.com>
2020-09-01 15:39:14 +0200

    contact api

--- routes/api.php
+++ routes/api.php
@@ -17,26 +17,22 @@ use Illuminate\Http\Request;
 // public ad endpoints
 Route::get('/ads', 'AdController@index');
 Route::get('/ads/{ad}', 'AdController@show');
-
+Route::post('/contact', 'ContactController@index');
 
 // auth endpoints (user&admin)
 Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
+  'middleware' => 'api',
+  'prefix' => 'auth'
 ], function () {
-
   Route::post('login', 'AuthController@login'); 
   Route::post('register', 'AuthController@register'); 
-
 });
 
 // protected endpoints
 Route::group(['middleware' => 'auth:api'], function () {
-
   Route::get('/me', 'AuthController@me');
   Route::get('/findMyAds', 'AdController@getByAuthUser');
   Route::post('/ads', 'AdController@store');
   Route::put('/updateAd/{ad}', 'AdController@update');
   Route::delete('/deleteAd/{ad}', 'AdController@destroy');
-
 });
